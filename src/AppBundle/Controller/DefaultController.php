a1fdf08441da33641c52e9e51aa1b8ef47e665b8
Haamida <hamida.meknessi@gmail.com>
2019-08-23 20:56:05 +0100

    Connect to rabbitmq and send test messages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use Enqueue\AmqpLib\AmqpConnectionFactory;
+use Interop\Amqp\AmqpQueue;
+use Interop\Amqp\AmqpTopic;
+use Interop\Amqp\Impl\AmqpBind;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,9 +18,48 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $context=$this->createOrUseConnection($this->getParameter('topic_rb'),$this->getParameter('queue_rb'));
+        $form = $this->createFormBuilder()
+            ->add('Send Request', SubmitType::class, ['attr' => ['class'=>'btn draw-border']])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $context->createMessage('test');
+            // Send message to queue
+            $context->createProducer()->send($this->createOrUseQueue($context, $this->getParameter('queue_rb')), $message);
+            return $this->render('default/index.html.twig',['form'=>$form->createView()]);
+        }
+        return $this->render('default/index.html.twig',['form'=>$form->createView()]);
+    }
+    /*
+     * Create or use an existant Context and topic
+     */
+    public function createOrUseConnection($topicName,$queueName){
+        $factory = new AmqpConnectionFactory([
+            'host' => $this->getParameter('host_rb'),
+            'port' => $this->getParameter('port_rb'),
+            'vhost' => $this->getParameter('vhost_rb'),
+            'user' => $this->getParameter('user_rb'),
+            'pass' => $this->getParameter('pass_rb'),
+            'persisted' => true,
         ]);
+
+
+        $context = $factory->createContext();
+        $topic = $context->createTopic($topicName);
+        $topic->setType(AmqpTopic::TYPE_DIRECT);
+        $context->declareTopic($topic);
+        $queue=$this->createOrUseQueue($context,$queueName);
+        $context->bind(new AmqpBind($topic, $queue));
+        return $context;
+    }
+    /*
+     * Create or use an existant Queue
+     */
+    public function createOrUseQueue($context,$queueName){
+        $fooQueue = $context->createQueue($queueName);
+        $fooQueue->addFlag(AmqpQueue::FLAG_DURABLE);
+        $context->declareQueue($fooQueue);
+        return $fooQueue;
     }
 }
